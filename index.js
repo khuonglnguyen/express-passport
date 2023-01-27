aa5b3ca43c1f5bedc6b88206a2ed48181753d65a
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-27 17:30:06 +0700

    Add page

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 
-const port = 1234;
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// GET: Home page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// GET: Login page
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+const port = 9090;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
